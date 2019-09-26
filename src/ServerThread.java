e41b93630a374d8a4369638efe2bbb157d9e0797
xaviRodri <xavier.rodriguez@betika.com>
2019-09-26 20:19:46 +0200

    New Server by Vitor
    Uses a basic Java server which allows the user to connect using user and password (program args).
    
    Example: `start -u user -p 1234`
    
    Added also a Logger which register the user's login.
    
    All is structured as a IntellIJ project.

---
+++ src/ServerThread.java
@@ -0,0 +1,19 @@
+import java.net.ServerSocket;
+import java.io.IOException;
+
+public class ServerThread implements Runnable {
+
+    /**
+     * Constructor, creates a new protocol object
+     * @param socket the socket used in the communication
+     * @throws IOException error creating the socket.
+     */
+    public ServerThread(ServerSocket socket) throws IOException {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
