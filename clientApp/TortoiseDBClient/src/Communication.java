fcae934b0c756d0fa77e2a7383b12c8104aec2c7
Vitor Carvalho <vitorthefogui@gmail.com>
2019-10-02 09:49:29 +0200

    Creating client app

---
+++ clientApp/TortoiseDBClient/src/Communication.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+import java.net.Socket;
+
+public class Communication {
+    private Protocol protocol;
+    private String serverMessage;
+    private boolean isRunning;
+    private TerminalInterface terminalInterface;
+
+    public Communication(Socket socket) throws IOException {
+        this.protocol           = new Protocol(socket);
+        this.serverMessage      = "";
+        this.isRunning          = true;
+        this.terminalInterface  = new TerminalInterface();
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public void setRunning(boolean running) {
+        isRunning = running;
+    }
+
+    public void startCommunication() throws IOException {
+        this.displayStartMessage();
+        this.readSocket();
+    }
+
+    private void displayStartMessage() {
+        this.terminalInterface.displayStartMessage("TortoiseDB");
+    }
+
+    private void getClientAction() throws IOException {
+
+    }
+
+    private void readSocket() throws IOException {
+        while (this.isRunning()) {
+            this.serverMessage = this.protocol.readServerMessage();
+
+            switch (this.serverMessage) {
+                //TODO: put server messages case here!..
+            }
+
+            this.getClientAction();
+        }
+    }
+}
